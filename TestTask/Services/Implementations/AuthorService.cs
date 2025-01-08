8cda5ba333f29dbbca258287a02fe3170913f022
Ivan <ivan.serdyukov270@gmail.com>
2025-01-08 21:02:30 +0300

    Add Author and Book service

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorService(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        /// <summary>
+        /// Returns the author who wrote the book with the longest title.
+        /// If there are several such authors, the author with the lowest Id is selected.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<Author> GetAuthor()
+        {
+            var longestBook = await _context.Books
+                .OrderByDescending(b => b.Title.Length)
+                .ThenBy(b => b.AuthorId)
+                .Select(b => new
+                {
+                    b.Author.Id,
+                    b.Author.Name,
+                    b.Author.Surname,
+                })
+                .FirstOrDefaultAsync();
+
+            if (longestBook == null)
+                return null;
+
+            return new Author
+            {
+                Id = longestBook.Id,
+                Name = longestBook.Name,
+                Surname = longestBook.Surname
+            };
+        }
+
+        /// <summary>
+        /// Returns a list of authors who have written an even number of books published after 2015.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<List<Author>> GetAuthors()
+        {
+            var authors = _context.Authors
+                .Where(author =>
+                    _context.Books
+                        .Where(book => book.AuthorId == author.Id && book.PublishDate.Year > 2015)
+                        .Count() % 2 == 0)
+                .ToListAsync();
+
+            return authors;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookService(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        /// <summary>
+        /// Get a single book with the highest published cost.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<Book> GetBook() => await _context.Books.OrderByDescending(b => b.Price * b.QuantityPublished).FirstOrDefaultAsync();
+
+        /// <summary>
+        /// Get a list of books that contain "Red" in the title and are published after "Carolus Rex" album release.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<List<Book>> GetBooks()
+        {
+            DateTime carolusRexReleaseDate = new DateTime(2012, 5, 25);
+
+            return await _context.Books
+                .Where(b => b.Title.Contains("Red") && b.PublishDate > carolusRexReleaseDate)
+                .ToListAsync();
+        }
+    }
+}
